141784b10ac7e078096acf5acd3e43bb5cf23acc
dragos.ismana <dragos.ismana@makingwaves.com>
2019-11-04 00:04:04 +0100

    completed part two of day 3

--- 2017/SpiralMemory/spiral.fsx
+++ 2017/SpiralMemory/spiral.fsx
@@ -54,8 +54,119 @@ let sideStart =
     |> Seq.find (fun (s, e) -> s < input && e > input ) 
     |> fst
 
-let position = input - sideStart + 1 // 17
+let position = input - sideStart + 1 
 let cartezianLen = sideLen / 2
 let offsetToCartezian = cartezianLen - position
 
-let total = offsetToCartezian + cartezianLen
+let totalMovement = offsetToCartezian + cartezianLen
+
+(*
+    --- Part Two ---
+    As a stress test on the system, the programs here clear the grid and then store the value 1 in square 1. Then, in the same allocation order as shown above, they store the sum of the values in all adjacent squares, including diagonals.
+
+    So, the first few squares' values are chosen as follows:
+
+    Square 1 starts with the value 1.
+    Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+    Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+    Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+    Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+    Once a square is written, its value does not change. Therefore, the first few squares would receive the following values:
+
+        6022  5600 5203  5022  2450
+    147  142  133  122   59    2391
+    304    5    4    2   57    2275
+    330   10    1    1   54    2105
+    351   11   23   25   26    1968 
+    362  747  806  880  931     957
+
+    What is the first value written that is larger than your puzzle input?
+
+    Input: 325489
+*)
+
+type Dir =
+    | R
+    | U
+    | L
+    | D
+
+let next = function
+    | R -> U
+    | U -> L
+    | L -> D
+    | D -> R
+
+let dirGenerator =
+    seq {
+        let mutable level = 1
+        let mutable dir = R
+        while true do
+            for _ in [1..2] do 
+                for _ in [1..level] do
+                    yield dir
+                dir <- next dir
+            level <- level + 1
+    }
+
+let nextPos currentPos dir = 
+    let (x,y) = currentPos
+    match dir with 
+    | R -> (x, y + 1) 
+    | U -> (x - 1, y)
+    | L -> (x, y - 1)
+    | D -> (x + 1, y)
+
+let spiralPositions startPos = 
+    seq {
+        yield startPos
+        let (x,y) = startPos
+        let mutable xm = x
+        let mutable ym = y
+        for dir in dirGenerator do
+            let (a, b) = nextPos (xm, ym) dir
+            xm <- a
+            ym <- b
+            yield (xm, ym)
+    }
+
+let neighbors (x, y) =
+    [
+        (x - 1, y - 1); (x - 1, y); (x - 1, y + 1);
+        (x    , y - 1);             (x    , y + 1);
+        (x + 1, y - 1); (x + 1, y); (x + 1, y + 1);
+    ]
+
+let sumOfNeighbors (matrix:int [][]) position = 
+    neighbors position
+    |> List.sumBy (fun (x,y) -> matrix.[x].[y] )
+
+let generateEmptyMatrix size = 
+    [|0..size|] 
+    |> Array.map (fun _ -> 
+        [|0..size|] |> Array.map (fun _ -> 0))
+
+let incremental (m:int[][]) i (x,y) =
+    m.[x].[y] <- (i+1)
+
+let sumByNeighbors (m:int[][]) i (x,y) =
+    if i = 0 
+        then m.[x].[y] <- 1
+    else
+    let sum = sumOfNeighbors m (x,y)
+    m.[x].[y] <- sum
+
+let generateSpiralMatrix how size =
+
+    let start = size / 2
+    let matrix = generateEmptyMatrix size 
+
+    spiralPositions (start, start)
+    |> Seq.take 100
+    |> Seq.iteri (fun i pos -> how matrix i pos)
+
+    matrix
+
+let generated = generateSpiralMatrix sumByNeighbors 12
+
+// 330785
