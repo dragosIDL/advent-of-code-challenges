d186b03ace307ab3c57cf03f89d9165a1e61f9ad
dragos.ismana <dragos.ismana@makingwaves.com>
2019-11-04 01:02:12 +0100

    implemented day 4

---
+++ 2017/high-entropy-passphrases/passphrases.fsx
@@ -0,0 +1,88 @@
+(*
+    --- Day 4: High-Entropy Passphrases ---
+    A new system policy has been put in place that requires all accounts to use a passphrase instead of simply a password. A passphrase consists of a series of words (lowercase letters) separated by spaces.
+
+    To ensure security, a valid passphrase must contain no duplicate words.
+
+    For example:
+
+    aa bb cc dd ee is valid.
+    aa bb cc dd aa is not valid - the word aa appears more than once.
+    aa bb cc dd aaa is valid - aa and aaa count as different words.
+    The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
+*)
+open System.IO
+
+let input = 
+    File.ReadAllLines "2017/high-entropy-passphrases/passphrases.txt"
+
+let isSeen (set:string Set) el = 
+    if set.Contains el then (set, true)
+    else (set.Add el, false)
+
+let isValidPassphrase (phrase:string) = 
+    let mutable holdingSet = Set.empty<string>
+
+    phrase.Split ' '
+    |> Seq.map (fun el -> 
+        let (set, v) = isSeen holdingSet el
+        holdingSet <- set
+        v)
+    |> Seq.exists id
+    |> not
+
+let result = 
+    input 
+    |> Array.filter isValidPassphrase
+    |> Array.length
+
+
+(*
+    --- Part Two ---
+    For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+
+    For example:
+
+    abcde fghij is a valid passphrase.
+    abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+    a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+    iiii oiii ooii oooi oooo is valid.
+    oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+    Under this new system policy, how many passphrases are valid?
+*)
+
+let rec distribute e = function
+  | [] -> [[e]]
+  | x::xs' as xs -> (e::xs)::[for xs in distribute e xs' -> x::xs]
+
+let rec permute = function
+  | [] -> [[]]
+  | e::xs -> List.collect (distribute e) (permute xs)
+
+let generatePermutations (str:string) =
+    str.ToCharArray() 
+    |> Array.toList
+    |> permute
+    |> List.distinct
+    |> List.map (List.toArray >> System.String)
+
+let isValidPermutationPassphrase (phrase:string) = 
+    let mutable holdingSet = Set.empty<string>
+
+    phrase.Split ' '
+    |> Array.map generatePermutations
+    |> Seq.collect id
+    |> Seq.map (fun el -> 
+        let (set, v) = isSeen holdingSet el
+        holdingSet <- set
+        v)
+    |> Seq.exists id
+    |> not
+
+let r1 = 
+    input 
+    |> Array.filter isValidPermutationPassphrase
+    |> Array.length
+
+// 208
+// a bit slow some optimization is in order
