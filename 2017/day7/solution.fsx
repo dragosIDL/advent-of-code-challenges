be1a1a8e46d5e66aa0e5765daf23cf2597b7dda1
dragos.ismana <dragos.ismana@makingwaves.com>
2020-03-13 22:01:40 +0100

    day7 solution

--- 2017/day7/solution.fsx
+++ 2017/day7/solution.fsx
@@ -52,3 +52,78 @@ let partOne =
     rows()
     |> Array.fold folder (Set.empty<string>,Set.empty<string>)
     |> fun (lfp,_) -> lfp |> Set.toSeq |> Seq.head
+
+
+// Part two
+type T = 
+    { Data: Data
+      Kids: T list }
+
+let toTree (row:Row) =
+    { Data=row.Data; Kids=[] }
+
+let treePairs (l:Row []) : Map<string, Row * T> =
+    l 
+    |> Array.map (fun row ->(row.Data.Name, (row, row |> toTree)))
+    |> Map.ofArray<string, Row * T>
+
+let rec buildTree start (map: Map<string, Row * T>) = 
+
+    let (row, tree) = map.Item start
+
+    let kids = 
+        row.Children
+        |> Array.map (fun ch -> buildTree ch map)
+        |> Array.toList
+
+    { tree with Kids = kids }
+
+let totalTree = buildTree partOne (treePairs (rows()))
+
+
+let balance node =
+    let totalNodeWeight =
+        node.Kids 
+        |> List.sumBy (fun el -> el.Data.Weight)
+        |> fun s -> s + node.Data.Weight
+
+    let diff =
+        node.Kids 
+        |> List.map (fun e -> e.Data.Weight)
+        |> List.distinct
+        |> fun l -> 
+            match l with
+            | [x] -> 0
+            | x::[y] -> absDiff x y
+            | _ -> failwith "impossible as per input"
+
+    (totalNodeWeight, diff)
+
+
+let rec balanced (rootNode:T) =
+
+    if rootNode.Kids.IsEmpty 
+    then 
+        rootNode.Data.Weight
+    else
+
+        let weights = 
+            rootNode.Kids 
+            |> List.map balanced
+        
+        let diff =
+            weights
+            |> List.distinct
+            |> fun l ->
+                match l with
+                | [x] -> 0
+                | x::[y] -> x - y
+                | _ -> failwith "impossible as per input"
+        
+        let v = 
+            if diff <> 0 
+            then printfn "diff : %i of  %A, kids: %A" diff rootNode.Data (rootNode.Kids |> List.map (fun k -> k.Data))
+        
+        rootNode.Data.Weight + ( weights |> List.sum)
+
+balanced totalTree
