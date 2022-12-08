57e54a2875bec077373beeba45f47626284032c9
dragos.ismana <dragos@techfuncs.com>
2022-12-08 10:55:29 +0100

    day8

---
+++ 2022/day8/csharp/Program.cs
@@ -0,0 +1,59 @@
+﻿var inputPath = "input.txt";
+
+var inputLines = File.ReadAllLines(inputPath);
+
+var matrix = inputLines
+    .Select(l => l.ToCharArray().Select(l => (int)l - 48).ToList())
+    .ToList();
+
+List<(int score, bool isVisible)> trees = new List<(int, bool)>();
+for (var i = 1; i < matrix.Count - 1; i++)
+    for (var j = 1; j < matrix[i].Count - 1; j++)
+        trees.Add(ComputeTree(matrix, i, j));
+
+(int, bool) ComputeTree(List<List<int>> matrix, int i, int j)
+{
+    var center = matrix[i][j];
+
+    var topTrees = Enumerable.Range(0, i).Select(index => matrix[i - 1 - index][j]).ToList();
+    var bottomTrees = Enumerable.Range(i + 1, matrix.Count - i - 1).Select(index => matrix[index][j]).ToList();
+    var leftTrees = Enumerable.Range(0, j).Select(index => matrix[i][j - 1 - index]).ToList();
+    var rightTrees = Enumerable.Range(j + 1, matrix[i].Count - j - 1).Select(index => matrix[i][index]).ToList();
+
+    var score = VisibleTrees(topTrees, center)
+        * VisibleTrees(bottomTrees, center)
+        * VisibleTrees(leftTrees, center)
+        * VisibleTrees(rightTrees, center);
+
+    var isVisible =
+        topTrees.Max() < center
+        || bottomTrees.Max() < center
+        || leftTrees.Max() < center
+        || rightTrees.Max() < center;
+
+    return (score, isVisible);
+}
+
+var rows = matrix[0].Count * 2;
+var sides = (matrix.Count - 2) * 2;
+
+Console.WriteLine($"The amount of visible trees is {trees.Count(a => a.isVisible) + rows + sides}");
+Console.WriteLine($"Max score {trees.Max(a => a.score)}");
+
+static int VisibleTrees(IEnumerable<int> el, int item)
+{
+    int l = 0;
+    foreach (var e in el)
+    {
+        if (e < item)
+        {
+            l++;
+        }
+        else
+        {
+            l++;
+            break;
+        }
+    }
+    return l;
+}
--- 2022/day8/solution.fs
+++
@@ -1,6 +0,0 @@
-open System
-open System.IO
-
-let inputPath ="2022\\day8\\input.txt"
-
-let input = File.ReadAllLines inputPath
