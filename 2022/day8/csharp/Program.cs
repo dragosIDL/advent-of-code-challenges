56fde252589eb190ea998256e8fcc00d676c3bd1
dragos.ismana <dragos@techfuncs.com>
2022-12-08 12:10:40 +0100

    ..

--- 2022/day8/csharp/Program.cs
+++ 2022/day8/csharp/Program.cs
@@ -11,6 +11,7 @@ for (var i = 1; i < matrix.Count - 1; i++)
     for (var j = 1; j < matrix[i].Count - 1; j++)
         trees.Add(ComputeTree(matrix, i, j));
 
+
 (int, bool) ComputeTree(List<List<int>> matrix, int i, int j)
 {
     var center = matrix[i][j];
@@ -37,23 +38,12 @@ for (var i = 1; i < matrix.Count - 1; i++)
 var rows = matrix[0].Count * 2;
 var sides = (matrix.Count - 2) * 2;
 
-Console.WriteLine($"The amount of visible trees is {trees.Count(a => a.isVisible) + rows + sides}");
-Console.WriteLine($"Max score {trees.Max(a => a.score)}");
+Console.WriteLine($"part1={trees.Count(a => a.isVisible) + rows + sides}");
+Console.WriteLine($"part2={trees.Max(a => a.score)}");
 
-static int CountVisibleTrees(IEnumerable<int> el, int item)
+static int CountVisibleTrees(List<int> allTrees, int currentTree)
 {
-    int l = 0;
-    foreach (var e in el)
-    {
-        if (e < item)
-        {
-            l++;
-        }
-        else
-        {
-            l++;
-            break;
-        }
-    }
-    return l;
+    var visibleTrees = allTrees.TakeWhile(i => i < currentTree).ToList();
+    var seesToTheEdge = visibleTrees.Count == allTrees.Count;
+    return visibleTrees.Count + (seesToTheEdge ? 0 : 1);
 }
